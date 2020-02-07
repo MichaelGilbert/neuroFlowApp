a8f7b9137940fc05476f2c4be4be7351ac5b822b
Michael Gilbert <mercsoft@comcast.net>
2020-02-07 10:36:45 -0500

    Story Two completed

---
+++ NeuroFlowProject/ColorCell.swift
@@ -0,0 +1,15 @@
+//
+//  ColorCell.swift
+//  NeuroFlowProject
+//
+//  Created by Michael Gilbert on 2/7/20.
+//  Copyright © 2020 Michael Gilbert. All rights reserved.
+//
+
+import UIKit
+
+class ColorCell: UICollectionViewCell {
+    
+    @IBOutlet weak var colorView: UIView!
+    
+}
--- NeuroFlowProject/FirstViewController.swift
+++ NeuroFlowProject/FirstViewController.swift
@@ -9,12 +9,58 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    let dataCount = 6
+    let colors: [UIColor] = [.black, .blue, .brown, .cyan, .red, .green]
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
 
+extension FirstViewController: UICollectionViewDelegate, UICollectionViewDataSource {
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        self.dataCount
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ColorCell", for: indexPath) as! ColorCell
+        
+        cell.colorView.backgroundColor = self.colors[indexPath.row]
+        return cell
+    }
+}
+
+extension FirstViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let collectionViewWidth = collectionView.bounds.width
+        let collectionViewHeight = collectionView.bounds.height
+        let numberOfRows:CGFloat = 4.0
+        var size:CGSize
+        
+        switch(indexPath.row) {
+        case 0, 5:
+            size = CGSize(width: collectionViewWidth, height: collectionViewHeight/numberOfRows)
+        case 1...4:
+            size = CGSize(width: collectionViewWidth/2, height: collectionViewHeight/numberOfRows)
+        
+        default:
+        size = CGSize(width: collectionViewWidth, height: collectionViewHeight/numberOfRows)
+        }
+        
+        return size
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
 }
 
