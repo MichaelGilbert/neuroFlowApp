e269f1a87a2cab03ecca132fa07852464290319f
Michael Gilbert <mercsoft@comcast.net>
2020-02-06 19:59:19 -0500

    Initial Commit

---
+++ NeuroFlowProject/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  NeuroFlowProject
+//
+//  Created by Michael Gilbert on 2/6/20.
+//  Copyright © 2020 Michael Gilbert. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
